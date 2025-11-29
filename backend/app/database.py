f09a8e05b0cd42c6cd4a038efe49ee5f0517665c
Maaz <siddiquimaaz94@gmail.com>
2025-11-30 02:39:32 +0500

    Revert "Complete Phase 2"
    This reverts commit 8dcd77499937f825361bf5cb118d9cd9a86cd878.

--- backend/app/database.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-from .config import settings
-
-client = MongoClient(settings.MONGODB_URI)
-db = client["PAKIndustryDB"]
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from .routes.company_routes import router as company_router
-from .routes.industry_routes import router as industry_router
 
-app = FastAPI(title="PAK Industry Insight API")
+app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "Backend API Working!"}
-
-app.include_router(company_router)
-app.include_router(industry_router)
+def read_root():
+    return {"message": "PAK Industry Insight API is running"}
